0652408ea1d8d46f46c65845d8652e1fe1677ec7
yacharenikrishnakanth <yacharenikrishnakanth@gmail.com>
2022-02-14 23:43:54 -0600

    initial commit

---
+++ src/main/java/com/electronics/vendor/ElectronicsVendorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.electronics.vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsVendorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsVendorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electronics/vendor/ElectronicsVendorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronics.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsVendorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
